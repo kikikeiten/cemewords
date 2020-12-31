2e7c66d5de5c093623aa7fcaab68b1e6fdfd3c5f
keiten.kiki <kikikeiten@gmail.com>
2020-12-31 18:09:46 +0900

    :card_file_box: Modify migration file

--- db/migrate/20201231090731_create_relationships.rb
+++ db/migrate/20201231090731_create_relationships.rb
@@ -1,10 +1,12 @@
-class CreateRelationships < ActiveRecord::Migration[6.1]
+class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :follow, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :follow, foreign_key: { to_table: :users }
 
       t.timestamps
+
+      t.index [:user_id, :follow_id], unique: true
     end
   end
 end
