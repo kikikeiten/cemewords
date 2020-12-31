f3838ae50a6562a0eee18c0cf04c62ba0ab202c9
keiten.kiki <kikikeiten@gmail.com>
2020-12-31 18:12:18 +0900

    :card_file_box: rails db:migrate

--- db/migrate/20201231090731_create_relationships.rb
+++ db/migrate/20201231090731_create_relationships.rb
@@ -1,4 +1,4 @@
-class CreateRelationships < ActiveRecord::Migration[5.2]
+class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
       t.references :user, foreign_key: true
