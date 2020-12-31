25f306a2491fffbd9bf6c8b89ccd5e4e58237552
keiten.kiki <kikikeiten@gmail.com>
2020-12-31 14:15:58 +0900

    :art: Add cemewords#index

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @cemeword = current_user.cemewords.build # form_with 用
+      @cemewords = current_user.cemewords.order(id: :desc).page(params[:page])
+    end
   end
 end
