8fe19d5a6e88e51781438e3d8f53ac5d2485634c
keiten.kiki <kikikeiten@gmail.com>
2020-12-31 17:33:50 +0900

    :sparkles: Add user's counts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @cemewords = @user.cemewords.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
