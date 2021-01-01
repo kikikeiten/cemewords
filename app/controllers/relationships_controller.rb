0504d9e8fa15aa4a6a971fa6620d907bbcfcb3c0
keiten.kiki <kikikeiten@gmail.com>
2021-01-01 14:37:00 +0900

    :sparkles: Edit controller of relationship (follow)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,17 @@
 class RelationshipsController < ApplicationController
+  before_action :require_user_logged_in
+
   def create
+    user = User.find(params[:follow_id])
+    current_user.follow(user)
+    flash[:success] = 'You followed the user.'
+    redirect_to user
   end
 
   def destroy
+    user = User.find(params[:follow_id])
+    current_user.unfollow(user)
+    flash[:success] = 'You unfollowed the user.'
+    redirect_to user
   end
 end
