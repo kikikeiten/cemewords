1c3540481fec4d5dfd13c8407a58a735f62eaa64
keiten.kiki <kikikeiten@gmail.com>
2021-01-01 20:36:21 +0900

    :wrench: Edit controller of top page

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @cemeword = current_user.cemewords.build # form_with 用
-      @cemewords = current_user.cemewords.order(id: :desc).page(params[:page])
+      @cemewords = current_user.feed_cemewords.order(id: :desc).page(params[:page])
     end
   end
 end
