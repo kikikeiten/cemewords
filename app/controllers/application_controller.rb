29987c558b6c25e32376023adb568cad85bb9d9d
keiten.kiki <kikikeiten@gmail.com>
2021-01-01 14:41:12 +0900

    :sparkles: Add following counts to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
 
   def counts(user)
     @count_cemewords = user.cemewords.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
