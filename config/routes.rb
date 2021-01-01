2e8508d26af552171f30efda8281bca0e4f58d71
keiten.kiki <kikikeiten@gmail.com>
2021-01-01 14:27:37 +0900

    :wrench: Add routes of follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'cemewords/create'
-  get 'cemewords/destroy'
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -8,7 +6,13 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
 
   resources :cemewords, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
